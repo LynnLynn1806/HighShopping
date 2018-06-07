fe9f4718d40ec3b874a9bc11ffe09528718e37dd
highshopping <paweena.int@highshopping.com>
2018-06-07 09:10:25 +0700

    login layout ver3

--- src/pages/forgot/forgot.ts
+++ src/pages/forgot/forgot.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import { AlertController } from 'ionic-angular';
 
 import {HomePage} from '../home/home';
 
@@ -12,8 +13,17 @@ export class ForgotPage {
     homePage = HomePage;
 
 
-  constructor(public navCtrl: NavController) {
+  constructor(public navCtrl: NavController,public alertCtrl: AlertController) {
 
+  }
+
+  confirm(){
+    let alert = this.alertCtrl.create({
+      title: 'Message',
+      subTitle: 'Message has been sent by your email. Please check your email.',
+      buttons: ['OK']
+    });
+    alert.present();
   }
 }
 
